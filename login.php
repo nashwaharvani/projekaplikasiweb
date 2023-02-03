7cceea3dfb9b9837d73c7224a4770a8f254fe7b3
nashwaharvani <124320552+nashwaharvani@users.noreply.github.com>
2023-02-03 11:20:36 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+//mengaktifkan session phpsession_start ();
+session_start();
+include 'koneksi.php';
+$username=$_POST['username'];
+$password=$_POST['password'];
+$login=mysqli_query($koneksi,"select * from admin where username='$username' and password='$password'");
+$cek =mysqli_num_rows($login);
+if($cek>0){
+	$SESSION['username'] = $username;
+	$SESSION['status'] = "login";
+header ("location:event/pendaftaran.php");}
+else{
+	header ("location:index.php?pesan=gagal");}
+?>
